a99dfc8c78ca745761914e7478913cca0544d429
Robert Lamberson <rlamberson@cnm.edu>
2021-11-11 14:24:35 -0600

    Added package.json

---
+++ public_html/documentation/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $("#contact").validate({
+        debug:true,
+        errorClass: "alert alert-danger",
+        errorLabelContainer: "#output-area",
+        errorElement: "div",
+        // rules here define what is good or bad input
+        // each rule starts with the form input element's NAME attributes
+        rules:{
+            name: {
+                required: true
+            },
+            email: {
+                email: true,
+                required: true
+            },
+            message: {
+                required: true,
+                maxlength: 2000,
+            }
+        },
+        message:{
+            name: {
+                required: "Please provide a name"
+            },
+            email: {
+                email: "Please provide a valid email address",
+                required: "Email is a required field"
+            },
+            message: {
+                required: "Please provide a message",
+                maxlength: "Message is to long"
+            }
+        },
+        submitHandler: (form) => {
+          $("#contact").ajaxSubmit({
+              type: "Post",
+              url: $("#contact").attr('action'),
+              success: (ajaxOutput) => {
+                  $("output-area").css("display", "")
+                  $("output-area").html(ajaxOutput)
+                  if($(".alert-sucess" >= 1)) {
+                      $("#contact")[0].reset()
+                  }
+              }
+          })
+        }
+    })
+})
